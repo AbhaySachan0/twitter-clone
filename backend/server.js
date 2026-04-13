7034487e87700b5097e8c48dbd3e51ceaa50978d
Abhay Sachan <abhaysach05@gmail.com>
2026-04-13 12:01:12 +0530

    add test routes and database connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("server is running...")
+import express from "express";
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+
+    connectMongoDB()
+})
