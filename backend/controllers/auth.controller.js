acaa5b8314a8362978be14b805900400389496d1
Abhay Sachan <abhaysach05@gmail.com>
2026-04-14 00:52:10 +0530

    add logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -95,7 +95,12 @@ export const login = async (req,res) => {
     }
 };
 export const logout = async (req,res) => {
-    res.json({
-        data: "You hit the logout endpoint"
-    })
+    try {
+        res.cookie("jwt","",({maxAge:0}));
+        res.status(200).json({message: "Logged out successfully"});
+    } catch (error) {
+        // console.log("ERROR:", error);
+        console.log( `Error in login controller : ${error.message}`)
+        res.status(500).json({error:"Internal Server Error"});
+    }
 };
