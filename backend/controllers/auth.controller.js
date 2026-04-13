31c5aa7a85730c5874b931341570cbbd764d80a8
Abhay Sachan <abhaysach05@gmail.com>
2026-04-14 00:48:35 +0530

    add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,13 +67,35 @@ export const signup = async (req,res) => {
 };
 
 export const login = async (req,res) => {
-    res.json({
-        data: "You hit the login endpoint"
-    })
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordValid = await bcrypt.compare(password, user?.password||"");
+        if(!user || !isPasswordValid) {
+            return res.status(400).json({error: "Invalid username or password"})
+
+        }
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullname : user.fullname,
+            email : user.email,
+            username : user.username,
+            followers : user.followers,
+            following : user.following,
+            profileImg : user.profileImg,
+            coverImg : user.coverImg,
+        })
+
+    } catch (error) {
+        // console.log("ERROR:", error);
+        console.log( `Error in login controller : ${error.message}`)
+        res.status(500).json({error:"Internal Server Error"});
+    }
 };
 export const logout = async (req,res) => {
     res.json({
         data: "You hit the logout endpoint"
     })
 };
-
